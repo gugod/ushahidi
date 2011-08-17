c37c13f3bc7687093fd3af242055a8e2d7c91a91
Kang-min Liu <gugod@gugod.org>
2011-08-17 21:40:47 +0800

    doodling some basic attributes

--- lib/ushahidi.rb
+++ lib/ushahidi.rb
@@ -1,5 +1,38 @@
-class Ushahidi
-  class Report
+require "doodle"
+
+module Ushahidi
+  class Incident < Doodle
+    has :title, :kind => String
+    has :description, :kind => String
+    has :categories, :init => []
+    has :at, :kind => Time
+
+    def to_a
+      return [
+        "incident_title", title,
+        "incident_description", description,
+        "incident_date", "%02d/%02d/%04d" % [ at.month, at.mday, at.year ],
+        "incident_hour", "%02d" % [at.hour % 12],
+        "incident_minute", "%02d" % at.min,
+        "incident_ampm", at.hour > 12 ? "pm" : "am",
+        "incident_category", categories.join(",")
+      ]
+    end
+  end
+    
+  class Report < Doodle
+    has :location_name, :kind => String
+    has :longitude, :kind => Float
+    has :latitude, :kind => Float
+    
+    def to_a
+      return [
+        "longitude", longitude.to_s,
+        "latitude", latitude.to_s,
+        "location_name", location_name
+      ]
+    end
+
     def post
     end
   end
--- spec/ushahidi_spec.rb
+++ spec/ushahidi_spec.rb
@@ -1,11 +1,57 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+# API Ref: http://wiki.ushahidi.com/doku.php?id=ushahidi_api
+
 describe "Ushahidi" do
 end
 
+describe "Ushahidi::Incident" do
+  it "has all the required fields" do
+    # incident_title - Required. The title of the incident/report.
+    # incident_description - Required. The description of the incident/report.
+    # incident_date - Required. The date of the incident/report. It usually in the format mm/dd/yyyy.
+    # incident_hour - Required. The hour of the incident/report. In the 12 hour format.
+    # incident_minute - Required. The minute of the incident/report.
+    # incident_ampm - Required. Is the incident/report am or pm. It of the form, am or pm.
+    # incident_category - Required. The categories the incident/report belongs to. It should be a comma separated value csv
+
+    x = Ushahidi::Incident.new(
+      :title => "XyZ",
+      :description => "Xyz Xyz Xyz",
+      :at => Time.utc(2000,5,5,10,5,0),
+      :categories => [1,2,3,5]
+      )
+
+    x.to_a.should == [
+      "incident_title", "XyZ",
+      "incident_description", "Xyz Xyz Xyz",
+      "incident_date", "05/05/2000",
+      "incident_hour", "10",
+      "incident_minute", "05",
+      "incident_ampm", "am",
+      "incident_category", "1,2,3,5"
+    ]
+  end
+end
+
 describe "Ushahidi::Report" do
+  before do
+    @report = Ushahidi::Report.new(
+      :longitude => 23.45,
+      :latitude => 123.45,
+      :location_name => "Somewhere Center"
+      )
+  end
+
   it "can be posted" do
-    u = Ushahidi::Report.new
-    u.respond_to?(:post).should be_true
+    @report.respond_to?(:post).should be_true
+  end
+
+  it "has those attribute accessors" do
+    @report.to_a.should == [
+      "longitude", "23.45",
+      "latitude", "123.45",
+      "location_name", "Somewhere Center"
+    ]
   end
 end
